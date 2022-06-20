125843b2f51f2592c6a51b7623d3bc87070eb4e0
Slackoth <00082318@uca.edu.sv>
2022-06-20 16:04:16 -0500

    Archivo de jugador 2 agregado. Comunicacion entre ambos jugadores lista

--- jugador-2.cpp
+++ jugador-2.cpp
@@ -12,7 +12,7 @@ int col;
 
 int main() {
     int aux = 1;
-    int jugadorUnoId;
+    int jugadorDosId;
     int tamanoMensaje;
 
     // Para red de mensajes
@@ -22,41 +22,53 @@ int main() {
     // Inicializar tablero
     // TODO
 
-    // Indicar que el jugador 1 ha empezado
-    enviar.tipo = 1;
-    enviar.filMat = 0;
-    enviar.colMat = 0;
-    enviar.tipoMensaje = 0;
-
     // Calcular tamano de mensaje
     tamanoMensaje = sizeof(mensaje) - sizeof(long);
 
     // Inicializar pila de mensajes
-    jugadorUnoId = msgget(LLAVE, 0600 | IPC_CREAT);
+    jugadorDosId = msgget(LLAVE, 0600 | IPC_CREAT);
 
-    // Mandar mensaje
-    msgsnd(jugadorUnoId, &enviar, tamanoMensaje, 0);
+    // Esperar hasta que el jugador 1 empiece
+    printf("Esperando al jugador 1...\n");
 
-    // Esperar hasta que el jugador 2 empiece
-    printf("Esperando al jugador 2...\n");
-
-    // Recibir mensaje del jugador 2
-    msgrcv(jugadorUnoId, &recibir, tamanoMensaje, 2, 0);
+    // Recibir confirmacion del jugador 1
+    msgrcv(jugadorDosId, &recibir, tamanoMensaje, 1, 0);
 
     // Salir si la conexion entre jugador 1 y 2 falla
     if(recibir.tipoMensaje != 0) {
         printf("ERROR: Conexion entre ambos jugadores fallo, vuelva a intentar.\n");
 
         // Eliminar pila de mensajes
-        msgctl(jugadorUnoId, IPC_RMID, 0);
+        msgctl(jugadorDosId, IPC_RMID, 0);
 
         return 0;
     }
+
+    // Indicar que el jugador 2 ha empezado
+    enviar.tipo = 2;
+    enviar.filMat = 0;
+    enviar.colMat = 0;
+    enviar.tipoMensaje = 0;
+
+    // Enviar mensaje
+    msgsnd(jugadorDosId, &enviar, tamanoMensaje, 0);
 
     // Mostrar tablero vacio
     // TODO
 
     // Colocar tropas en tablero
+    // TODO
+
+    // Esperar movimiento del jugador 1
+    printf("Esperando al jugador 1...\n");
+
+    // Recibir primer movimiento del jugador 1
+    msgrcv(jugadorDosId, &recibir, tamanoMensaje, 0, 0);
+
+    // Reflejar movimiento del jugador 1
+    // TODO
+
+    // Mostrar tablero
     // TODO
 
     // Inicializar juego
