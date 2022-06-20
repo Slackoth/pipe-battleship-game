df67b26ed0796762aac675b6d8988c79148cfc65
Slackoth <00082318@uca.edu.sv>
2022-06-20 14:00:03 -0500

    Comentarios en mensaje.h

--- mensaje.h
+++ mensaje.h
@@ -1,5 +1,16 @@
 #ifndef MENSAJE_H
 #define MENSAJE_H
+#define LLAVE 34856 
+
+// tipo:
+// 		0 = ver "tipoMensaje"
+// 		1 = jugador 1 ha empezado.
+// 		2 = jugador 2 ha empezado.
+// tipoMensaje:
+// 		0 = Esperar al otro jugador.
+// 		1 = Turno del otro jugador.
+// 		2 = Ganador jugador 1.
+// 		3 = Ganador jugador 2.
 
 typedef struct Mensaje {
     long tipo;
